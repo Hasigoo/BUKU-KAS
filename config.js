167bc0d6a99467dd69997012b29c424b5d241042
hasigoo <cv.hasigoo@gmail.com>
2026-09-17 01:26:22 +0700

    Update Supabase configuration with new URL and key

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Gunakan Publishable key (sb_publishable_...) atau anon key lama.
 // JANGAN memasukkan secret/service_role key di file ini.
 window.SUPABASE_CONFIG = {
-  url: "https://PROJECT-ANDA.supabase.co",
-  key: "ISI_PUBLISHABLE_ATAU_ANON_KEY"
+  url: "https://yvesadksyrapruhxfwic.supabase.co/rest/v1/",
+  key: "sb_publishable_BX-7zGceph4YTQNahZBv8w_PUCZ3NYG"
 };
