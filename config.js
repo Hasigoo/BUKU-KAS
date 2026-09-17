ae8c6082cec774a9c84ed2ba9dce5d0a39061a44
hasigoo <cv.hasigoo@gmail.com>
2026-09-17 09:20:22 +0700

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Gunakan Publishable key (sb_publishable_...) atau anon key lama.
 // JANGAN memasukkan secret/service_role key di file ini.
 window.SUPABASE_CONFIG = {
-  url: "https://yvesadksyrapruhxfwic.supabase.co/rest/v1/",
+  url: "https://github.com/Hasigoo/BUKU-KAS.git",
   key: "sb_publishable_BX-7zGceph4YTQNahZBv8w_PUCZ3NYG"
 };
